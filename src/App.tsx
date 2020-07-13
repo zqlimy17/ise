a96ee31637967aab5b420482246f2d61871dbbf2
zqlimy17 <zqlimy@gmail.com>
2020-07-13 17:09:35 +0800

    mapped products

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
+import axios from "axios";
 import "./styles/styles.css";
+import Product from "./components/Product";
 
 const App: FC = () => {
-    return <div className='App'></div>;
+    const [products, setProducts] = useState<object[]>([
+        {
+            image:
+                "http://s3-ap-southeast-1.amazonaws.com/s3.irvinsaltedegg.com/engineering-test/images/product-1.jpg",
+            price: "15",
+            currency: "SGD",
+            name: "IPhone 7",
+        },
+    ]);
+    const [loaded, setLoaded] = useState(false);
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    const fetchProducts = async () => {
+        let response = await axios.get(
+            "http://s3.irvinsaltedegg.com.s3-ap-southeast-1.amazonaws.com/engineering-test/products.json"
+        );
+        setProducts(response.data.data);
+        setLoaded(true);
+    };
+    return (
+        <div className='App'>
+            {loaded
+                ? products!.map((product: any, index: number) => {
+                      return (
+                          <div key={index}>
+                              <Product product={product} />
+                          </div>
+                      );
+                  })
+                : ""}
+        </div>
+    );
 };
 
 export default App;
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,7 +1,23 @@
 import React, { FC } from "react";
 
-const Product: FC = () => {
-    return <div></div>;
+interface ProductInterface {
+    product: {
+        image: string;
+        price: number;
+        currency: string;
+        name: string;
+    };
+}
+
+const Product: FC<ProductInterface> = ({ product }) => {
+    return (
+        <div>
+            <img src={product.image} alt={product.name} />
+            <p>Name: {product.name}</p>
+            <p>Price: {product.price}</p>
+            <p>Currency: {product.currency}</p>
+        </div>
+    );
 };
 
 export default Product;
