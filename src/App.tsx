8a2f4a2530469d6fb3a6b1598ace161c6c87d482
zqlimy17 <zqlimy@gmail.com>
2020-07-13 17:24:14 +0800

    shopping cart and hr

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import React, { FC, useState, useEffect } from "react";
+import Product from "./components/Product";
 import axios from "axios";
 import "./styles/styles.css";
-import Product from "./components/Product";
+
+import GridLoader from "react-spinners/GridLoader";
 
 const App: FC = () => {
-    const [products, setProducts] = useState<object[]>([
-        {
-            image:
-                "http://s3-ap-southeast-1.amazonaws.com/s3.irvinsaltedegg.com/engineering-test/images/product-1.jpg",
-            price: "15",
-            currency: "SGD",
-            name: "IPhone 7",
-        },
-    ]);
+    const [products, setProducts] = useState<object[]>();
+    const [cart, setCart] = useState(2);
     const [loaded, setLoaded] = useState(false);
+
     useEffect(() => {
-        fetchProducts();
+        setTimeout(() => {
+            fetchProducts();
+        }, 3000);
     }, []);
 
     const fetchProducts = async () => {
@@ -25,17 +23,47 @@ const App: FC = () => {
         setProducts(response.data.data);
         setLoaded(true);
     };
+
+    const addToCart = () => {
+        // add to cart
+    };
     return (
         <div className='App'>
-            {loaded
-                ? products!.map((product: any, index: number) => {
-                      return (
-                          <div key={index}>
-                              <Product product={product} />
-                          </div>
-                      );
-                  })
-                : ""}
+            {loaded ? (
+                products!.map((product: any, index: number) => {
+                    return (
+                        <div key={index}>
+                            <Product product={product} />
+                        </div>
+                    );
+                })
+            ) : (
+                <div className='loader'>
+                    <GridLoader color={"#efb784"} />
+                </div>
+            )}
+
+            <div>
+                <hr />
+            </div>
+
+            {loaded ? (
+                <div>
+                    <h1>
+                        SHOPPING CART{" "}
+                        {cart ? (
+                            <span>
+                                {" "}
+                                - {cart} item{cart > 1 ? "s" : ""}
+                            </span>
+                        ) : (
+                            ""
+                        )}
+                    </h1>
+                </div>
+            ) : (
+                ""
+            )}
         </div>
     );
 };
